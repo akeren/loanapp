8ba2471dce070d4c1fd3845d676202f018870f54
Kater Akeren <akerenkater@gmail.com>
2019-07-21 10:53:12 +0100

    Created **UI** variables, Added event listener and Calculate **Loan** results

---
+++ app.js
@@ -0,0 +1,34 @@
+// UI Variables 
+const loanForm = document.querySelector('#loan-form');
+const amount = document.querySelector('#amount');
+const interest = document.querySelector('#interest');
+const years = document.querySelector('#years');
+const monthlyPayment = document.querySelector('#monthly-payment');
+const totalPayment = document.querySelector('#total-payment');
+const totalInterest = document.querySelector('#total-interest');
+
+
+const calculateRessults = (e) => {
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+    const k = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * k * calculatedInterest) / (k - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+
+        console.log('All fields are required!');
+    }
+
+    e.preventDefault();
+};
+
+
+loanForm.addEventListener('submit', calculateRessults);
